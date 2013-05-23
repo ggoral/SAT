725f99f0dcf1d2ea891fe8ff8550d575e291b01a
Christian Tracy <christianetracy@hotmail.com>
2013-05-22 23:37:38 -0300

    Cambié el contenido del index

--- Index.php
+++ Index.php
@@ -1,5 +1,34 @@
-<?php
-
-echo "hola lucho";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+    <title>Prueba BootStrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"> 
+	<link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen"> 
+	
+  </head>
+  <body>
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner"><div class="container"><a href="#" class="brand">unTexto</a></div></div>
+	</div>
+  </body>
+  
+  
+<script src="js/jquery-1.9.1"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="../assets/js/jquery.js"></script>
+<script src="../assets/js/bootstrap-transition.js"></script>
+<script src="../assets/js/bootstrap-alert.js"></script>
+<script src="../assets/js/bootstrap-modal.js"></script>
+<script src="../assets/js/bootstrap-dropdown.js"></script>
+<script src="../assets/js/bootstrap-scrollspy.js"></script>
+<script src="../assets/js/bootstrap-tab.js"></script>
+<script src="../assets/js/bootstrap-tooltip.js"></script>
+<script src="../assets/js/bootstrap-popover.js"></script>
+<script src="../assets/js/bootstrap-button.js"></script>
+<script src="../assets/js/bootstrap-collapse.js"></script>
+<script src="../assets/js/bootstrap-carousel.js"></script>
+<script src="../assets/js/bootstrap-typeahead.js"></script>
+</html>
