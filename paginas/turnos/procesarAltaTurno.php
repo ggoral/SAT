20b264aec3df6d16e6e642c66afe65a664744957
Christian Tracy <christianetracy@hotmail.com>
2013-05-27 22:20:51 -0300

    probando

--- paginas/turnos/procesarAltaTurno.php
+++ paginas/turnos/procesarAltaTurno.php
@@ -1,11 +1,12 @@
-<?php include "../conectar.php";
-$dni = $_POST["dni"];
-$query = "select * from persona as per
-          where $dni = per.dni 
-          innerjoin paciente as pa on (pa.id = per.id)";
-$result=  mysql_query($query);
-while ($row = mysql_fetch_array($result)){
+<?php
+//include "../conectar.php";
+//$dni = $_POST["dni"];
+//$query = "select * from persona as per
+//          where $dni = per.dni 
+//          innerjoin paciente as pa on (pa.id = per.id)";
+//$result=  mysql_query($query);
+//while ($row = mysql_fetch_array($result)){
 ?>
 <span class="text-error"><small>El paciente ingresado no existe!<i class="icon-chevron-right"></i></small></span>
 <a class="btn btn-danger btn-small" href="#"> Agregar Paciente</a><p></p>
-<?php }?>
+<?php // }?>
