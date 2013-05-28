710a1e43d4489f11aec892cdb1a5ddd366605b7e
Christian Tracy <christianetracy@hotmail.com>
2013-05-27 23:23:39 -0300

    funciona el verificar paciente

--- paginas/turnos/procesarAltaTurno.php
+++ paginas/turnos/procesarAltaTurno.php
@@ -1,13 +1,16 @@
 <?php
 include "../conectar.php";
 $dni = $_GET["dni"];
+
+if ($dni === ""){ $dni = 0;} // ESTO LO PONGO PROVISORIO PARA QUE SI VIENE VACIO PONGA UN 0
+
 $query = "select * from persona 
         where dni = $dni
             ";
 $result=  mysql_query($query);
 if(mysql_num_rows($result) != 0){
 ?>
-<p><span class="text-success"> El dni ingresado es correcto </span><i class="icon-ok"></i></p>
+<p><span class="text-success"> El DNI ingresado es correcto </span><i class="icon-ok"></i></p>
 <?php
 }
 else{
