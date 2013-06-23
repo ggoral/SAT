8628cc9f528dda9aa9fee3a67e2e989984ff55a6
LuchoSantos <luchhh@gmail.com>
2013-06-22 23:58:54 -0300

    me confundi

--- paginas/conectar.php
+++ paginas/conectar.php
@@ -1,9 +1,9 @@
 <?php
     $host = 'localhost';
-    $usuario = 'root';
-    $contraseña = 'root';
+    $usuario = '';
+    $contraseña = '';
     $db ='ingenieria';  
     mysql_connect($host, $usuario, $contraseña) or die (mysql_error());
     mysql_select_db($db);
-    //mysql_query("SET NAMES 'utf8'");
+    mysql_query("SET NAMES 'utf8'");
 ?>
