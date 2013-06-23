b2c6b3cb51f60c609a7d348e60ff33418f46e2da
Luciano Santos <Luchhh@gmail.com>
2013-06-22 23:50:42 -0300

    especialidad

--- paginas/conectar.php
+++ paginas/conectar.php
@@ -1,9 +1,9 @@
 <?php
     $host = 'localhost';
-    $usuario = '';
-    $contraseña = '';
+    $usuario = 'root';
+    $contraseña = 'root';
     $db ='ingenieria';  
     mysql_connect($host, $usuario, $contraseña) or die (mysql_error());
     mysql_select_db($db);
-    mysql_query("SET NAMES 'utf8'");
+    //mysql_query("SET NAMES 'utf8'");
 ?>
