e1777617939c3bd1d81600ac88bea0571ca38412
Christian Tracy <christianetracy@hotmail.com>
2013-06-26 02:30:46 -0300

    correct

--- js/medicoAlta.js
+++ js/medicoAlta.js
@@ -1,22 +1,4 @@
 (document).ready(function() {// INICIO DOCUMENT READY
 
-
-   //Carga las Localidades segun la provincia elegida------------------------------
-   
-    $("#provincia").change(function(){
-      
-        var data = "provincia=" +  this.value;
-        $.ajax({
-            type: "POST",
-            url: "cargarLocalidades.php",
-            async: false,
-            data: data,
-            success: function(html) {
-                $("#localidad").empty();
-                $("#localidad").append(html);
-            }
-        });
-    });
-   
 }); //FIN DOCUMENT READY
     
