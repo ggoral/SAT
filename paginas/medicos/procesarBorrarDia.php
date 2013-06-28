8fbd06e6641ed529dce6de22151adb39b4a4ee0a
David <David@David-PC>
2013-06-28 00:40:20 -0300

    ajustes al borrar horarios

--- paginas/medicos/procesarBorrarDia.php
+++ paginas/medicos/procesarBorrarDia.php
@@ -2,11 +2,11 @@
 
 include '../conectar.php';
 $dia = $_GET["id"];
-
+$idMedico = $_GET["idM"];
 $borrar = "DELETE FROM diasatencion WHERE id='$dia'";
 mysql_query($borrar);
 
 
-header('location: listar.php');
+header("location: diasAtencion.php?id=$idMedico");
     
 ?>
